89330cfdff3d1baa55c8c68a81ba7fc72213fa84
gauravdewan <gauravdewan@live.com>
2025-05-27 23:02:18 +0100

    added default openAI key

--- core-sparta/app/config.py
+++ core-sparta/app/config.py
@@ -1,7 +1,7 @@
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    openai_api_key: str
+    openai_api_key: str = "sk-"
     environment: str = "development"
     debug: bool = False
 
